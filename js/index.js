22ab78ca948b847bcdb073bf4a5cb0eda566598a
romanlrmk <romanlrmk@gmail.com>
2021-05-25 18:38:55 +0300

    JS Countdown

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
+/* Special Offer Countdown*/
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
 const dealExpires = "1 Jan 2022";
 
 function countdown() {
-    const dealExpiresDate = new Date(dealExpires);
-    const currentDate = new Date();
+  const dealExpiresDate = new Date(dealExpires);
+  const currentDate = new Date();
+
+  const totalSeconds = (dealExpiresDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
 }
 
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
